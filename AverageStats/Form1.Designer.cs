7e969be4a4d5ed7188378b67e7fa0550f85707d7
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-10-19 14:02:55 -0400

    Averages - First Commit

---
+++ AverageStats/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace AverageStats
+{
+    partial class FrmAverageStats
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnGetInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnGetInfo
+            // 
+            this.BtnGetInfo.Location = new System.Drawing.Point(362, 86);
+            this.BtnGetInfo.Name = "BtnGetInfo";
+            this.BtnGetInfo.Size = new System.Drawing.Size(145, 53);
+            this.BtnGetInfo.TabIndex = 0;
+            this.BtnGetInfo.Text = "Get Average Stats";
+            this.BtnGetInfo.UseVisualStyleBackColor = true;
+            this.BtnGetInfo.Click += new System.EventHandler(this.BtnGetInfo_Click);
+            // 
+            // FrmAverageStats
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnGetInfo);
+            this.Name = "FrmAverageStats";
+            this.Text = "Average Stats";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnGetInfo;
+    }
+}
+
---
+++ AverageStats/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AverageStats
+{
+    public partial class FrmAverageStats : Form
+    {
+        public FrmAverageStats()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGetInfo_Click(object sender, EventArgs e)
+        {
+            double age;
+            int count = 0;
+            double height;
+            double totalAge = 0;
+            double totalHeight = 0;
+            double averageAge = 0;
+            double averageHeight = 0;
+            string response = "";
+
+            while (true)
+            {
+                do
+                {
+                    response = Microsoft.VisualBasic.Interaction.InputBox("do you wish to continue y/n", "Averages", "", 200, 200);
+                }
+                while (response != "y" && response != "n");
+
+                if (response == "n")
+                    break;
+
+                age = Convert.ToDouble(Microsoft.VisualBasic.Interaction.InputBox("Enter an age between 13 and 18 ", "Averages", "", 200, 200));
+                if (age < 13 || age > 18)
+                {
+                    MessageBox.Show("Invalid Age");
+                }
+                else
+                {
+                    count++;
+                    totalAge += age;
+                }
+
+                height = Convert.ToDouble(Microsoft.VisualBasic.Interaction.InputBox("Enter height in cenemeters ", "Averages", "", 200, 200));
+                if (height < 1)
+                { 
+                    MessageBox.Show("Invalid Age");
+                }
+                else
+                {
+                    totalHeight += height;
+                }
+
+            }
+
+            if (count != 0)
+            {
+                averageAge = totalAge / count;
+                averageHeight = totalHeight / count;
+                MessageBox.Show("The average age is " + averageAge.ToString("n2") 
+                    + "\n" + "The average cenemeters is " + averageHeight.ToString("n2"));
+            }
+            else
+            {
+                MessageBox.Show("No marks entered");
+            }
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FrmWhileTrue_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+    
+}
