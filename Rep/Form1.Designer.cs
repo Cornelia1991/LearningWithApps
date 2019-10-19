e96f3aa3546883cc9cd4116cf6809ad3ee65baba
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-10-19 09:19:11 -0400

    Do While Example - First Commit

---
+++ Rep/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Rep
+{
+    partial class FrmRep
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnRepEx = new System.Windows.Forms.Button();
+            this.BtnCounter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnRepEx
+            // 
+            this.BtnRepEx.Location = new System.Drawing.Point(226, 42);
+            this.BtnRepEx.Name = "BtnRepEx";
+            this.BtnRepEx.Size = new System.Drawing.Size(134, 40);
+            this.BtnRepEx.TabIndex = 0;
+            this.BtnRepEx.Text = "Rep Example";
+            this.BtnRepEx.UseVisualStyleBackColor = true;
+            this.BtnRepEx.Click += new System.EventHandler(this.BtnRepEx_Click);
+            // 
+            // BtnCounter
+            // 
+            this.BtnCounter.Location = new System.Drawing.Point(226, 130);
+            this.BtnCounter.Name = "BtnCounter";
+            this.BtnCounter.Size = new System.Drawing.Size(141, 46);
+            this.BtnCounter.TabIndex = 1;
+            this.BtnCounter.Text = "Counter Loop";
+            this.BtnCounter.UseVisualStyleBackColor = true;
+            this.BtnCounter.Click += new System.EventHandler(this.BtnCounter_Click);
+            // 
+            // FrmRep
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 387);
+            this.Controls.Add(this.BtnCounter);
+            this.Controls.Add(this.BtnRepEx);
+            this.Name = "FrmRep";
+            this.Text = "Rep Intro";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnRepEx;
+        private System.Windows.Forms.Button BtnCounter;
+    }
+}
+
---
+++ Rep/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rep
+{
+    public partial class FrmRep : Form
+    {
+        public FrmRep()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRepEx_Click(object sender, EventArgs e)
+        {
+            // User Controlled Loop
+            while (true)
+            {
+                MessageBox.Show("This is a test");
+
+                string response = Microsoft.VisualBasic.Interaction.InputBox("Do you want to continue y/n", "","");
+
+                if (response == "n")
+                    break;
+            }
+        }
+
+        private void BtnCounter_Click(object sender, EventArgs e)
+        {
+            //Counter loop
+            int counter = 0;
+
+            while (counter <= 10)
+            {
+                MessageBox.Show("The counter is on number " + counter.ToString());
+                counter++;
+
+            }
+        }
+    }
+}
